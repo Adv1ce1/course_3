06bf6b4c52efbc4c2fd252bb91c54255f622a2a9
Egor <egor.aksenov.2019@mail.ru>
2023-06-17 16:20:31 +0300

    Initial commit

---
+++ utils.py
@@ -0,0 +1,13 @@
+from zipfile import ZipFile
+import json
+
+
+def open_file_in_zip(path_zip: str) -> list[dict]:
+    """
+    Открывает файл внутри ZIP-архива и возвращает его содержимое в формате JSON
+    """
+    with ZipFile(path_zip, "r") as myzip:
+        for item in myzip.infolist():
+            if item.filename.endswith(".json"):
+                with myzip.open(item.filename, "r") as f:
+                    return json.load(f)
